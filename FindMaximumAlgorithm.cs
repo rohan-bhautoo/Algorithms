9d5e9424b6a0a655488360c2a1c98be91ab1b7c3
Rohan Bhautoo <47154593+rohan-bhautoo@users.noreply.github.com>
2024-02-22 20:51:00 +0400

    Create FindMaximumAlgorithm.cs

---
+++ FindMaximumAlgorithm.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class BubbleSortAlgorithm
+{
+    public static void Main(string[] args)
+    {
+        int[] arr1 = {2, 0, 6, 9, 1, 8, 4};
+        Console.WriteLine(FindMaximum(arr1));
+    }
+    
+    public static int FindMaximum(int[] arr)
+    {
+        int N = arr.Length;
+        int max = int.MinValue;
+        
+        for(int i = 0; i < N; i++)
+        {
+            if(arr[i] > max)
+                max = arr[i];
+        }
+        
+        return max;
+    }
+}
