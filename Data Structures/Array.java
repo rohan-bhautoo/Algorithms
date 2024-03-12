25ebce75b20e8daa76ab1b8bc2fc0ef46fe14f29
Rohan Bhautoo <47154593+rohan-bhautoo@users.noreply.github.com>
2024-03-12 14:51:15 +0400

    Update Array.java

--- Data Structures/Array.java
+++ Data Structures/Array.java
@@ -1,3 +1,55 @@
 class Array {
+    private int[] items;
+    private int count;
     
+    public Array(int length) {
+        this.items = new int[length];
+    }
+    
+    public void print() {
+        for(int i = 0; i < count; i++) {
+            System.out.println(items[i]);
+        }
+    }
+    
+    public void insert(int item) {
+        // If array is full, resize it
+        if(items.length == count) {
+            // Create a new array (twice the size)
+            int[] newItems = new int[count * 2];
+            
+            // Copy all the existing items
+            for(int i = 0; i < count; i++) {
+                newItems[i] = items[i];
+            }
+            
+            // Set "items" to this new array
+            items = newItems;
+        }
+        
+        // Add new item at the end
+        items[count++] = item;
+    }
+    
+    public void removeAt(int index) {
+        // Validate index
+        if(index < 0 || index >= count)
+            throw new IllegalArgumentException();
+        
+        // Shift items to the left to fill the hole
+        for(int i = index; i < count; i++) {
+            items[i] = items[i + 1];
+        }
+        
+        count--;
+    }
+    
+    public int indexOf(int item) {
+        for(int i = 0; i < count; i++) {
+            if(items[i] == item)
+                return i;
+        }
+        
+        return -1;
+    }
 }
